69a9072815eb708a6ccf677b6343b92aad865a16
Hirayasmeenfullstackdeveloper <hirayasmeen1106@gmail.com>
2025-04-21 14:19:55 +0500

    assignment1

---
+++ assign1/main.py
@@ -0,0 +1,50 @@
+print("Hellow World")
+
+print("Hira Yasmeen")
+
+name = "Hira"
+age = 19
+print("My name is:", name)
+print(age)
+
+def giaic():
+    
+    print("Hira Amna")
+    print("Welcome to Q3")
+giaic()
+
+name : str = ("GIAIC = Governor House")
+print("GIAIC stand for:", name)
+
+var_str = "Hira"
+print(var_str)
+id_card = """Name: Hira Yasmeen
+Father name: Abdul Wahid
+age: 20
+"""
+print(id_card)
+
+num :int = 45
+print(type(num))
+
+float_num :float = 3.7
+print(type(float_num))
+
+is_true :bool = True
+is_false :bool = False 
+print(type(is_true))
+
+fruit:list[str] =["apple","cherry"]
+print(type(fruit))
+print(fruit[0])
+print(fruit[-1])
+
+tuple_data:tuple = ("apple","cherry")
+print(tuple_data)
+
+user_data : dict[str, str] = {
+    "name" : "Hira Yasmeen",
+    "father name" : "Abdul Wahid",
+    "age" : "34"
+}
+print(user_data)
